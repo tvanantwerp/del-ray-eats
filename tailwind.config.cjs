0336e1c8177eee90f2a94c4313a7509638ee5597
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-01-08 17:40:35 -0500

    Work on styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,4 +3,11 @@ module.exports = {
     "./public/**/*.html",
     "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}",
   ],
+  theme: {
+    fontFamily: {
+      sans: '"Nunito", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      serif:
+        '"Playfair Display", ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+    },
+  },
 };
