1c5a40ea735cae9207af1e1500e3bd9f0b826636
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-09-07 22:22:34 -0400

    Set up prettier

--- src/scripts/imagesProcessing.js
+++ src/scripts/imagesProcessing.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
-const fsExtra = require("fs-extra");
-const path = require("path");
-const sharp = require("sharp");
-const restaurants = require("../data/restaurants.json");
-const images = require("../data/images.json");
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const path = require('path');
+const sharp = require('sharp');
+const restaurants = require('../data/restaurants.json');
+const images = require('../data/images.json');
 
-const INPUT_DIR = path.resolve(__dirname, "../data/images");
-const OUTPUT_DIR = path.resolve(__dirname, "../../public/images");
+const INPUT_DIR = path.resolve(__dirname, '../data/images');
+const OUTPUT_DIR = path.resolve(__dirname, '../../public/images');
 const FORMATS = images.formats;
 const RESOLUTIONS = images.resolutions;
 
@@ -23,30 +23,30 @@ function pathToImage(filename) {
 }
 
 function processImage(filename) {
-  FORMATS.forEach((format) => {
-    RESOLUTIONS.forEach((resolution) => {
+  FORMATS.forEach(format => {
+    RESOLUTIONS.forEach(resolution => {
       sharp(pathToImage(filename))
         .resize({ width: resolution[0], height: resolution[1] })
         .toFile(
           path.resolve(
             OUTPUT_DIR,
-            `${filename}-${resolution[0]}-${resolution[1]}.${format}`
+            `${filename}-${resolution[0]}-${resolution[1]}.${format}`,
           ),
           (err, info) => {
             if (err) {
               console.error(`Error while processing ${filename}.png`, err);
             } else {
               console.log(
-                `Success: ${filename}.png at ${info.width}x${info.height} in ${info.format} format`
+                `Success: ${filename}.png at ${info.width}x${info.height} in ${info.format} format`,
               );
             }
-          }
+          },
         );
     });
   });
 }
 
-restaurants.forEach((restaurant) => {
+restaurants.forEach(restaurant => {
   const imagePath = path.resolve(INPUT_DIR, `${restaurant.slug}.png`);
   if (fs.existsSync(imagePath)) {
     processImage(restaurant.slug);
