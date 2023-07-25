de0a876a35decb383e45beedfdeac9baca965ff6
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-07-25 13:59:26 -0400

    Use Astro Picture component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
+import image from '@astrojs/image';
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import serviceWorker from 'astrojs-service-worker';
 
 export default defineConfig({
-  integrations: [react(), serviceWorker()],
+  integrations: [
+    react(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    serviceWorker(),
+  ],
 });
